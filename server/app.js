e6c817583bafe24a58fdbbe8d58df2295c018823
Nikolay Varlamov <varlamow.col@yahoo.com>
2021-05-12 22:54:52 +0300

    server update

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+let bodyParser = require('body-parser')
+
+// Permit the app to parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Use body-parser as middleware for the app.
+app.use(bodyParser.json());
+
 const port = 65432
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+    console.log('Req Ip: ', req.ip, 'Req body:', req.body);
+});
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+    console.log('Req Ip: ', req.ip, 'Req body:', req.body);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
